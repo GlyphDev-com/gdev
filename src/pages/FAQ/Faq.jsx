cbf414379145edc02c6c985f2e525d0295a23edf
gamo-88 <maximegamo@gmail.com>
2025-03-08 16:13:03 +0100

    faq page

--- src/pages/FAQ/Faq.jsx
+++ src/pages/FAQ/Faq.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import Header from "../../components/common/header";
 import Space from "../../components/common/space";
 import Footer from "../../components/common/footer";
+import { useTranslation } from "react-i18next";
 
 export default function Faq() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Header />
@@ -11,21 +14,15 @@ export default function Faq() {
 
       <section className="container wh-2 ">
         <article style={{ margin: "32px" }}>
-          <p>
-            Vous avez des questions concernant nos services de développement
-            d'applications mobile/web et de pages web ? Nous avons rassemblé ici
-            les réponses aux questions les plus courantes. Si vous ne trouvez
-            pas ce que vous cherchez, n'hésitez pas à nous contacter
-            directement.
-          </p>
+          <p>{t("faqIntroduction")}</p>
         </article>
 
         <section
-        //   style={{ margin: "32px", padding: "32px", overflow: "scroll",display:"flex", flexDirection:"column", gap:"35px" }}
-        className="d-flex flex-column m-4 gap-3 "
+          //   style={{ margin: "32px", padding: "32px", overflow: "scroll",display:"flex", flexDirection:"column", gap:"35px" }}
+          className="d-flex flex-column m-4 gap-3 "
         >
-          <article >
-            <h1>Services et Expertise</h1>
+          <article>
+            <h1>{t("faqServiceEtExpertise")}</h1>
             <div className="accordion" id="service">
               <div class="accordion-item">
                 <h2 class="accordion-header">
@@ -37,7 +34,7 @@ export default function Faq() {
                     aria-expanded="true"
                     aria-controls="type"
                   >
-                    Quels types d’applications développez-vous ?
+                    {t("quelsType")}
                   </button>
                 </h2>
                 <div
@@ -46,9 +43,7 @@ export default function Faq() {
                   data-bs-parent="#type"
                 >
                   <div class="accordion-body">
-                    Nous développons des applications mobiles pour iOS et
-                    Android, des applications web et des pages web sur mesure,
-                    adaptées aux besoins spécifiques de votre entreprise.
+                    {t("quelsTypeRe")}
                   </div>
                 </div>
               </div>
@@ -63,7 +58,7 @@ export default function Faq() {
                     aria-expanded="false"
                     aria-controls="compentences"
                   >
-                    Quelles sont vos principales compétences ?
+                    {t("quelsCompetences")}
                   </button>
                 </h2>
                 <div
@@ -72,10 +67,7 @@ export default function Faq() {
                   data-bs-parent="#service"
                 >
                   <div class="accordion-body">
-                    Nous sommes spécialisés dans le développement d’applications
-                    mobiles natives, hybrides, ainsi que dans la création de
-                    sites web interactifs et responsives en utilisant des
-                    technologies comme React, Vue.js, Angular, etc.
+                    {t("quelsCompetencesRe")}
                   </div>
                 </div>
               </div>
@@ -83,7 +75,7 @@ export default function Faq() {
           </article>
 
           <article>
-            <h1>Développement et Processus</h1>
+            <h1>{t("faqDeveloppementEtProcessus")}</h1>
             <div className="accordion" id="developpement">
               <div class="accordion-item">
                 <h2 class="accordion-header">
@@ -95,20 +87,16 @@ export default function Faq() {
                     aria-expanded="true"
                     aria-controls="dev"
                   >
-                    Comment fonctionne le processus de développement d'une
-                    application ?
+                    {t("CommentFonctionneLeProcessus")}
                   </button>
                 </h2>
                 <div
                   id="dev"
-                  className="accordion-collapse collapse show"
+                  className="accordion-collapse collapse"
                   data-bs-parent="#developpement"
                 >
                   <div class="accordion-body">
-                    Nous suivons une méthode agile, avec une première phase de
-                    consultation pour comprendre vos besoins, puis des phases de
-                    conception, de développement, de tests et de mise en
-                    production. Nous vous tenons informé à chaque étape.
+                    {t("CommentFonctionneLeProcessusRe")}
                   </div>
                 </div>
               </div>
@@ -123,7 +111,7 @@ export default function Faq() {
                     aria-expanded="false"
                     aria-controls="processus"
                   >
-                    Combien de temps faut-il pour développer une application ?
+                    {t("combienDeTemps")}
                   </button>
                 </h2>
                 <div
@@ -132,9 +120,7 @@ export default function Faq() {
                   data-bs-parent="#developpement"
                 >
                   <div class="accordion-body">
-                    Le temps de développement dépend de la complexité de votre
-                    projet. En moyenne, une application mobile peut prendre
-                    entre 2 et 6 mois.
+                    {t("combienDeTempsRe")}
                   </div>
                 </div>
               </div>
@@ -142,7 +128,7 @@ export default function Faq() {
           </article>
 
           <article>
-            <h1>Tarification</h1>
+            <h1>{t("faqTarification")}</h1>
             <div className="accordion" id="tarification">
               <div class="accordion-item">
                 <h2 class="accordion-header">
@@ -154,19 +140,16 @@ export default function Faq() {
                     aria-expanded="true"
                     aria-controls="tarif"
                   >
-                    Comment déterminez-vous vos tarifs ?
+                    {t("commentDeterminerLeTarif")}
                   </button>
                 </h2>
                 <div
                   id="tarif"
-                  className="accordion-collapse collapse show"
+                  className="accordion-collapse collapse"
                   data-bs-parent="#tarification"
                 >
                   <div class="accordion-body">
-                    Nos tarifs sont basés sur la complexité du projet, les
-                    technologies nécessaires et le temps estimé pour la
-                    réalisation. Nous proposons des devis personnalisés après
-                    avoir évalué vos besoins.
+                    {t("commentDeterminerLeTarifRe")}
                   </div>
                 </div>
               </div>
@@ -181,7 +164,7 @@ export default function Faq() {
                     aria-expanded="false"
                     aria-controls="forfait"
                   >
-                    Proposez-vous des forfaits fixes ou des tarifs horaires ?
+                    {t("proposezDesForfaits")}
                   </button>
                 </h2>
                 <div
@@ -190,9 +173,7 @@ export default function Faq() {
                   data-bs-parent="#tarification"
                 >
                   <div class="accordion-body">
-                    Nous offrons les deux options. Selon la taille et la durée
-                    de votre projet, nous vous proposerons un devis au forfait
-                    ou un tarif horaire.
+                    {t("proposezDesForfaitsRe")}
                   </div>
                 </div>
               </div>
@@ -200,7 +181,7 @@ export default function Faq() {
           </article>
 
           <article>
-            <h1>Maintenance et Support</h1>
+            <h1>{t("faqMaintenanceEtSupport")}</h1>
             <div className="accordion" id="maintenance">
               <div class="accordion-item">
                 <h2 class="accordion-header">
@@ -212,18 +193,16 @@ export default function Faq() {
                     aria-expanded="true"
                     aria-controls="maintain"
                   >
-                    Offrez-vous un service de maintenance après la livraison ?
+                    {t("offrerUnService")}
                   </button>
                 </h2>
                 <div
                   id="maintain"
-                  className="accordion-collapse collapse show"
+                  className="accordion-collapse collapse "
                   data-bs-parent="#maintenance"
                 >
                   <div class="accordion-body">
-                    Oui, nous offrons des services de maintenance pour corriger
-                    les bugs, effectuer des mises à jour ou ajouter de nouvelles
-                    fonctionnalités.
+                    {t("offrerUnServiceRe")}
                   </div>
                 </div>
               </div>
@@ -238,7 +217,7 @@ export default function Faq() {
                     aria-expanded="false"
                     aria-controls="support"
                   >
-                    Comment gérez-vous le support après le lancement ?
+                    {t("commentGererLeSupport")}
                   </button>
                 </h2>
                 <div
@@ -247,19 +226,15 @@ export default function Faq() {
                   data-bs-parent="#maintenance"
                 >
                   <div class="accordion-body">
-                    Nous proposons une assistance technique continue, que ce
-                    soit par email, téléphone ou chat en ligne. Nous assurons
-                    aussi des mises à jour régulières pour garder vos
-                    applications à jour.
+                    {t("commentGererLeSupportRe")}
                   </div>
                 </div>
               </div>
             </div>
           </article>
 
-
           <article>
-            <h1>Livraison et Tests</h1>
+            <h1>{t("faqLivraisonEtTest")}</h1>
             <div className="accordion" id="livraison">
               <div class="accordion-item">
                 <h2 class="accordion-header">
@@ -271,16 +246,16 @@ export default function Faq() {
                     aria-expanded="true"
                     aria-controls="maintain"
                   >
-                    Comment puis-je tester l’application avant la livraison ?
+                    {t("commentPuisJeTester")}
                   </button>
                 </h2>
                 <div
                   id="how"
-                  className="accordion-collapse collapse show"
+                  className="accordion-collapse collapse show "
                   data-bs-parent="#livraison"
                 >
                   <div class="accordion-body">
-                    Nous offrons des versions beta de l’application pour que vous puissiez tester les fonctionnalités avant la mise en ligne officielle. Nous effectuons aussi des tests utilisateurs pour garantir la qualité du produit.
+                    {t("commentPuisJeTesterRe")}
                   </div>
                 </div>
               </div>
